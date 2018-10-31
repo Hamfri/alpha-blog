d416915018dbc5e995da4136c9c12b76df38b6ef
Hamfri <kibzham@gmail.com>
2018-10-31 12:00:26 +0300

    Signup users from UI

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   get root 'pages#home'
   get 'about', to: 'pages#about'
   resources :articles
+  #users routes
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new] #because we have the new route already
 end
